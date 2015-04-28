dccbf85e1f96b6bb44c64a0d3f7393db43e140b0
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-04-28 13:06:02 -0700

    Alright I guess this is the right version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://code.kyoto.io/qpfiffer/olegdb-python',
     name='olegdb-python',
     description='Python wrappers for generic OlegDB methods.',
-    version='0.1.2',
+    version='0.1.4',
     license='BSD',
     keywords='OlegDB',
     packages=['olegdb'],
